8457c21fe9da6e25491976cbf63697b43732b601
Zoo Wie <de-roo.dirk@hotmail.com>
2021-01-06 13:06:02 +0100

    Update currency.php
    changed some currency values to check if it works

--- currency.php
+++ currency.php
@@ -1,24 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>currency</title>
 </head>
-
 <body>
-	<form action="currency.php" method="GET">
-		Enter Currency : <input type="text" name="input">
-		Select Currency:<select name="dropdown">
-			<option value="usd">Us Dollars</option>
-			<option value="yen">Yen</option>
-			<option value="gdp">British Pounds</option>
-		</select>
-		<input type="submit" name="sbmt" value="convert">
+<form  action="currency.php" method="GET">
+Enter Currency : <input type="text" name="input">
+Select Currency:<select name= "dropdown">
+<option value="usd">Us Dollars</option>
+<option value="yen">Yen</option>
+<option value="gdp">British Pounds</option>
+</select>
+<input type="submit" name="sbmt" value="convert">
 
 </body>
-
 </html>
 
 <?php
@@ -27,16 +24,19 @@ if (isset($_GET['sbmt']))
 	$input = $_GET['input'];
 	$dd_dropdown =$_GET['dropdown'];
 	if($dd_dropdown=='usd'){
-		$dd_output =$input * 1.23379;
+		$dd_output =$input * 1.5;
 		echo  $dd_output ;
 	}
 	else if($dd_dropdown =='yen'){
-		$dd_output =$input * 1.9999;
+		$dd_output =$input * 2;
 		echo  $dd_output ;
 	}
 	else if ($dd_dropdown=='gdp'){
 		$dd_output =$input * 2.5;
 		echo  $dd_output ;
-	}	
+	}
+
+	
 }
+
 ?>
