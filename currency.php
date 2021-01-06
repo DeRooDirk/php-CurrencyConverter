f3c1d2f4018e295d7a5afb88167364cc7965b928
Zoo Wie <de-roo.dirk@hotmail.com>
2021-01-06 17:55:52 +0100

    update.php
    reversed the calculations

--- currency.php
+++ currency.php
@@ -3,40 +3,47 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
 	<title>currency</title>
 </head>
 <body>
-<form  action="currency.php" method="GET">
-Enter Currency : <input type="text" name="input">
-Select Currency:<select name= "dropdown">
-<option value="usd">Us Dollars</option>
-<option value="yen">Yen</option>
-<option value="gdp">British Pounds</option>
-</select>
-<input type="submit" name="sbmt" value="convert">
-
-</body>
-</html>
-
-<?php
+	<div class="container">
+		<div>
+		<img src="./pngwing.com.png" alt="">
+		</div>
+		<form  action="currency.php" method="GET">
+			<h1>  Currency Converter</h1>
+			<h2>Enter Currency : <input type="text" name="input"></h2>
+			<h2>Select Currency:<select name= "dropdown"></h2>
+				<option value="usd">Us Dollars</option>
+				<option value="yen">Yen</option>
+				<option value="gdp">British Pounds</option>
+			</select>
+			<div class="btndiv">
+			<input  class="btn "type="submit" name="sbmt" value="convert to euro's">
+			</div>
+			<?php
 if (isset($_GET['sbmt']))
 {
 	$input = $_GET['input'];
 	$dd_dropdown =$_GET['dropdown'];
 	if($dd_dropdown=='usd'){
-		$dd_output =$input * 1.5;
-		echo  $dd_output ;
+		$dd_output =$input * 0.81;
+		echo "<h3>$input dollars converts to $dd_output  euro</h3>";
+		
 	}
 	else if($dd_dropdown =='yen'){
-		$dd_output =$input * 2;
-		echo  $dd_output ;
+		$dd_output =$input * 0.0079;
+		echo "<h3>$input yen converts to $dd_output  euro</h3>";
 	}
 	else if ($dd_dropdown=='gdp'){
-		$dd_output =$input * 2.5;
-		echo  $dd_output ;
-	}
-
-	
+		$dd_output =$input * 1.1;
+		echo "<h3>$input pounds converts to $dd_output  euro</h3>";
+	}	
 }
-
 ?>
+	</div>
+
+</body>
+</html>
+
